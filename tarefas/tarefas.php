457b65d3f0c53f5ad8f0b47afc0b98197b4d1766
AlanCesarAraujo <alancesar389@gmail.com>
2017-10-10 11:35:47 -0300

    Gerenciador de Tarefas

---
+++ tarefas/tarefas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gerenciador de Tarefas</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Gerenciador de Tarefas</h1>
+
+	<form>
+		<fieldset>
+			<legend>Nova Tarefa</legend>
+			<label>
+				Tarefa:
+				<input type="text" name="nome" />
+			</label>
+			<input type="submit" value="Cadastrar" />
+		</fieldset>
+	</form>
+
+	<?php
+		$lista_tarefas = array();
+
+		if(isset($_GET['nome'])){
+			$lista_tarefas[] = $_GET['nome'];
+		}
+	?>
+
+	<TABLE>
+		<tr>
+			<th>Tarefas</th>
+		</tr>
+
+		<?php foreach ($lista_tarefas as $tarefa) : ?>
+			<tr>
+				<td><?php echo $tarefa; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</TABLE>
+	
+</body>
+</html>
